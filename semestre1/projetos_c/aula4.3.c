81931a629442c12266e55fc67e332cbc76f8ad67
Victor.Manzini <victor@robertomanzini.com.br>
2026-03-24 11:39:04 -0300

    Aula do dia 24/03/2026

---
+++ semestre1/projetos_c/aula4.3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+// Parte 3 da aula 4 
+// 24/03/2026
+
+int main(){
+    int n1 = 10;
+    int n2 = 3; 
+
+    printf("%f\n", (float)n1/n2);
+
+    return 0;
+}
---
+++ semestre1/projetos_c/aula4.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#include <string.h>
+// Aula sobre variáveis em C 
+// 24/03/2026
+int main(){
+    int idade; 
+
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+    printf("Sua idade é: %d\n", idade);
+
+    return 0;
+
+}
